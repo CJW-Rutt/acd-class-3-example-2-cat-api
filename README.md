49db38b64a7ad5883af7146f8081fb58c1ebc793
Corey J <cjw.rutt@gmail.com>
2024-01-19 11:18:52 -0800

    Initial Upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Cat API using Axios
+
+## Installations
+npm i axios
+
+## API 
+[The Cat API](https://thecatapi.com/)
---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Views from '@/components/Views';
+
+export default function Home() {
+
+  const apiKey = process.env.NEXT_PUBLIC_API;
+
+  const url = `https://api.thecatapi.com/v1/images/search?limit=10&api_key=${apiKey}`;
+
+  const [data, setData] = useState<ICat[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(url);
+      console.log(response.data);
+      setData(response.data);
+    }
+
+    getData()
+    
+    .catch(console.error);    // to catch any errors
+  }, [])
+
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+    >
+     {
+        data && data.map((d, index) => {
+          return(
+            <div key={index}>
+              <Views path={d.url} alternative={d.id} height={d.height} width={d.width} />
+            </div>
+          )
+        })
+     } 
+    </main>
+  )
+}
